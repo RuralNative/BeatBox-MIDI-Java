95c1d985d58b2c7e4cef1e483c3d2018fd49d591
John Berlin Leonor <117552434+RuralNative@users.noreply.github.com>
2023-06-10 21:05:06 +0800

    Initialized Creation of GUI Build Method
    Along with the standard code for the creation of the main class, such as the main method, the imports, and its attributes, an initial code for the GUI build method is written.

---
+++ Beatbox/src/Main.java
@@ -0,0 +1,31 @@
+import javax.sound.midi.Sequence;
+import javax.sound.midi.Sequencer;
+import javax.sound.midi.Track;
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+
+public class BeatBox {
+    JPanel mainPanel;
+    JFrame theFrame;
+    ArrayList<JCheckBox> checkBoxList;
+    Sequencer sequencer;
+    Sequence sequence;
+    Track track;
+
+    public static void main(String[] args) {
+    }
+
+    public void buildGUI() {
+        theFrame = new JFrame("Cyber BeatBox");
+        theFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        BorderLayout layout = new BorderLayout();
+        JPanel background = new JPanel(layout);
+        background.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+
+        checkBoxList = new ArrayList<JCheckBox>();
+        Box buttonBox = new Box(BoxLayout.Y_AXIS);
+
+        JButton button = new JButton("Start");
+    }
+}
