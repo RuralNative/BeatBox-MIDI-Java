ce36b46766fc2cb1e200c0b7ffe5094c45edd4bc
John Berlin Leonor <117552434+RuralNative@users.noreply.github.com>
2023-06-10 20:50:12 +0800

    Initialized Main Class
    Imported the necessary imports for the set attributes of the main class, with an empty main method

---
+++ Beatbox/src/BeatBox.java
@@ -0,0 +1,18 @@
+import javax.sound.midi.Sequence;
+import javax.sound.midi.Sequencer;
+import javax.sound.midi.Track;
+import javax.swing.*;
+import java.util.ArrayList;
+
+public class BeatBox {
+        JPanel mainPanel;
+        JFrame theFrame;
+        ArrayList<JCheckBox> checkBoxList;
+        Sequencer sequencer;
+        Sequence sequence;
+        Track track;
+
+    public static void main(String[] args) {
+        
+    }
+}
