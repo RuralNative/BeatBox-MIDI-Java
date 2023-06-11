85a884868cb205819d8dec06fa492477ad558d15
John Berlin Leonor <117552434+RuralNative@users.noreply.github.com>
2023-06-11 08:56:30 +0800

    Modified buildGUI() with Comments
    Added informative comments to the blocks of code of the buildGUI() to enable readability due to lack of documentation and in preparation for reorganization later. Also added a method at the end to add the JPanel to the application JFrame

--- Beatbox/src/Main.java
+++ Beatbox/src/Main.java
@@ -19,31 +19,43 @@ public class BeatBox {
     }
 
     public void buildGUI() {
+        //Instantiates the JFrame of the Application with necessary properties
         theFrame = new JFrame("Cyber BeatBox");
         theFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //Instantiates the JPanel with BorderLayout and Border
         BorderLayout layout = new BorderLayout();
         JPanel background = new JPanel(layout);
         background.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
 
+        //Instantiates CheckBox List
         checkBoxList = new ArrayList<JCheckBox>();
+
+        //Instantiate Box to contain Buttons
         Box buttonBox = new Box(Y_AXIS);
 
+        //Instantiates and adds Buttons to our Box (buttonBox)
         JButton start = new JButton("Start");
-
+        buttonBox.add(start);
         JButton stop = new JButton("Stop");
-        
+        buttonBox.add(stop);
         JButton upTempo = new JButton("Tempo Up");
-
+        buttonBox.add(upTempo);
         JButton downTempo = new JButton("Tempo Down");
+        buttonBox.add(downTempo);
 
+        //Instantiates Box to contain 16 labels created from a for loop and based from an array of String of instrument names
         Box nameBox = new Box(Y_AXIS);
         for (int i = 0; i < 16; i++) {
             nameBox.add(new Label(instrumentNames[i]));
         }
 
+        //Add all containers - buttonBox and nameBox - to our JPanel (background)
         background.add(BorderLayout.EAST, buttonBox);
         background.add(BorderLayout.WEST, nameBox);
 
+        //Add JPanel to JFrame
+        theFrame.getContentPane().add(background);
 
     }
 }
