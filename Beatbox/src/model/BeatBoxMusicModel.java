e0322b68b1a6487e03788fea90c075e89c9bd0db
John Berlin Leonor <117552434+RuralNative@users.noreply.github.com>
2023-06-12 11:08:45 +0800

    Added Getter Method for Music Model
    Added the getter method for the music model for one of its attributes which is the integer array named instruments

--- Beatbox/src/model/BeatBoxMusicModel.java
+++ Beatbox/src/model/BeatBoxMusicModel.java
@@ -9,4 +9,8 @@ public class BeatBoxMusicModel {
     Sequence sequence;
     Track track;
     int[] instruments = {35, 42, 46, 38, 49, 39, 50, 60, 70, 72, 64, 56, 58, 47, 67, 63};
+
+    public int[] getInstruments() {
+        return instruments;
+    }
 }
