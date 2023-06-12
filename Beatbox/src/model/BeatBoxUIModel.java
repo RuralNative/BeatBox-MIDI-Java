b1177eb1ea8fe0f8106dc63638381bca4fbea3bc
John Berlin Leonor <117552434+RuralNative@users.noreply.github.com>
2023-06-12 12:03:38 +0800

    Provided Value for UI Model Attribute
    Provided a value to be used for all presenter classes for the attribute of the UI Model class which is the checkBoxList

--- Beatbox/src/model/BeatBoxUIModel.java
+++ Beatbox/src/model/BeatBoxUIModel.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 
 public class BeatBoxUIModel {
     String[] instrumentNames = {"Bass Drum", "Closed Hi-hat", "Open Hi-hat", "Acoustic Snare", "Crash Cymbal", "Hand Clap", "High Tom", "High Bongo", "Maracas", "Whistle", "Low Conga", "Cowbell", "Vibraslap", "Low-mid Tom", "High Agogo", "Open High Conga"};
-    ArrayList<JCheckBox> checkBoxList = null;
+    ArrayList<JCheckBox> checkBoxList = new ArrayList<JCheckBox>();
 
     protected String[] getInstrumentNames() {
         return instrumentNames;
