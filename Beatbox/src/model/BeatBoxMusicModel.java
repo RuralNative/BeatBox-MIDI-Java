a8ff7d8d975fe57d10392d44fc25dd95f193d673
John Berlin Leonor <117552434+RuralNative@users.noreply.github.com>
2023-06-12 10:15:48 +0800

    Set Attributes for the Model Classes
    Set attributes for the Music and UI model classes of the applications, ensuring that the models serve separate concerns

--- Beatbox/src/model/BeatBoxMusicModel.java
+++ Beatbox/src/model/BeatBoxMusicModel.java
@@ -1,4 +1,12 @@
 package model;
 
+import javax.sound.midi.Sequence;
+import javax.sound.midi.Sequencer;
+import javax.sound.midi.Track;
+
 public class BeatBoxMusicModel {
+    Sequencer sequencer;
+    Sequence sequence;
+    Track track;
+    int[] instruments = {35, 42, 46, 38, 49, 39, 50, 60, 70, 72, 64, 56, 58, 47, 67, 63};
 }
--- Beatbox/src/model/BeatBoxUIModel.java
+++ Beatbox/src/model/BeatBoxUIModel.java
@@ -1,4 +1,11 @@
 package model;
 
+import javax.swing.*;
+import java.util.ArrayList;
+
 public class BeatBoxUIModel {
+    JPanel mainPanel;
+    JFrame theFrame;
+    String[] instrumentNames = {"Bass Drum", "Closed Hi-hat", "Open Hi-hat", "Acoustic Snare", "Crash Cymbal", "Hand Clap", "High Tom", "High Bongo", "Maracas", "Whistle", "Low Conga", "Cowbell", "Vibraslap", "Low-mid Tom", "High Agogo", "Open High Conga"};
+    ArrayList<JCheckBox> checkBoxList;
 }
